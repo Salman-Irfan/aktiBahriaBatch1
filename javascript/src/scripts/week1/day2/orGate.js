8f0e11e871519a8c1a7bc2477b86f6262cb8ae19
Salman Irfan <107026575+Salman-Irfan@users.noreply.github.com>
2024-03-03 15:10:13 +0500

    while loop

---
+++ javascript/src/scripts/week1/day2/orGate.js
@@ -0,0 +1,12 @@
+console.log(`and or gate`)
+
+let temperature = 45 // bahir garmi hai 
+
+let isRaining = false // bahir baarish ho rhi hai
+
+
+if (temperature > 40 || isRaining) {
+    console.log(`jao ja k baarish enjoy karo`) 
+} else {
+    console.log(`stay home stay safe`)
+}
---
+++ javascript/src/scripts/week1/day2/while.js
@@ -0,0 +1,10 @@
+let count = 0
+
+while (count <= 10) {
+    console.log(`while loop is counting conditionally: ${count}`)
+    console.log(`agr ham ghalati se b while loop ki condition mei increment krna bhool jayen
+    to kia progam finish ho ga?
+        kahan finish GainNode, program to warr jaye ga
+    `)
+    count++
+}
